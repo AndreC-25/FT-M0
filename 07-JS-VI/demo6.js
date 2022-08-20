7d37a009f94dd14b8db979a7509f2e2acb7ca3e7
André Cabrejos <andrecabrejos.25@gmail.com>
2022-08-20 02:52:48 -0500

    Homework 6

---
+++ 07-JS-VI/demo6.js
@@ -0,0 +1,70 @@
+// FUNCIONES
+
+function decirHolaAlUsuario(usuario) {
+        return 'Hola ' + usuario + '!';
+}
+
+function decirAdiosAlUsuario(usuario) {
+        return 'Adiós ' + usuario + '!';
+}
+
+
+function crearSaludo(usuario, cb) {
+        return cb(usuario);
+}
+
+
+// console.log(decirHolaAlUsuario('Eduardo'))
+// console.log(decirAdiosAlUsuario('Eduardo'))
+
+
+// crearSaludo('Dan', decirHolaAlUsuario); // 'Hello Dan!'
+// crearSaludo('Dan', decirAdiosAlUsuario);
+
+
+
+// ARREGLOS
+
+var alumnos = ['mati', 'leo', 'tincho', 'emi', 'jimmy', 'franco'];
+var edadesAlumnos = [20, 23, 22, 20, 24, 25];
+
+
+// sin callbacks
+
+// for (var i = 0; i < alumnos.length; i++) {
+//         if(alumnos[i] === 'tincho') return;
+//         console.log(alumnos[i]); 
+// }
+
+
+// function mostrarAlumno(alumno) {
+//         console.log('hola ' + alumno)
+// }
+
+
+alumnos.forEach(
+        function(elemento, i) {
+                // console.log('hola desde el forEach ' + elemento + ' ' + i)
+        }
+)
+
+
+var resultadoMap = alumnos.map( function(alumno, i) {       
+        return 'Hola desde Map ' + alumno + ' ' + i      
+} )
+
+// console.log(resultadoMap)
+
+
+var resultadoReduce = edadesAlumnos.reduce( function(acumulador, item, i) {
+        if(item === 23 || i === 0) {
+
+        } else {
+                acumulador = acumulador + item
+        }
+        return acumulador;
+}, 0)
+
+console.log(resultadoReduce)
+
+
